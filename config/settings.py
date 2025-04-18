daafd4a123173270799b4469aff5b0125a3add78
Franco Benko <fran.benko@gmail.com>
2025-04-17 23:05:59 -0300

    add the proper import method for the reddit api and update the wrapper.

--- config/settings.py
+++ config/settings.py
@@ -9,8 +9,23 @@ load_dotenv()
 FINANCIAL_NEWS_API_KEY = os.environ['fin_news_key']
 FINANCIAL_NEWS_API_URL = os.environ['fin_news_host']
 
-REDDIT_API_KEY = os.environ['reddit_key']
-REDDIT_API_URL = os.environ['reddit_host']
+
+REDDIT_CLIENT_ID = os.environ['reddit_client_id']
+REDDIT_CLIENT_KEY = os.environ['reddit_client_key']
+
+REDDIT_USR_GRANT_TP = os.environ['reddit_usr_grant_type']
+REDDIT_USR_USERNAME = os.environ['reddit_usr_username']
+REDDIT_USR_PASSWORD = os.environ['reddit_usr_password']
+
+REDDIT_ACCESS_URL = os.environ['reddit_access_url']
+
+REDDIT_HOST = os.environ['reddit_host']
+
+
+
+
+
+
 
 COINGECKO_API_KEY = os.environ['coingeko_key']
 COINGECKO_API_URL = os.environ['coingeko_host']
--- data/reddit_api.py
+++ data/reddit_api.py
@@ -1,22 +1,60 @@
 # data/reddit_api.py
 import requests
-from config.settings import REDDIT_API_URL
+from config.settings import REDDIT_CLIENT_ID, REDDIT_CLIENT_KEY, REDDIT_USR_GRANT_TP, REDDIT_USR_USERNAME, REDDIT_USR_PASSWORD, REDDIT_ACCESS_URL, REDDIT_HOST
+
 
 class RedditAPI:
     @staticmethod
     def get_posts(ticker: str, start_time: str, end_time: str) -> list:
         """Fetch Reddit posts mentioning the ticker."""
-        params = {
-            "q": ticker,
-            "after": start_time,
-            "before": end_time,
-            "size": 50
+        # Authenticacion mediante request
+        auth = requests.auth.HTTPBasicAuth(REDDIT_CLIENT_ID, REDDIT_CLIENT_KEY)
+
+
+        # Datos del usuario de Reddit
+        data = { 
+            'grant_type': REDDIT_USR_GRANT_TP,
+            'username' : REDDIT_USR_USERNAME,
+            'password' : REDDIT_USR_PASSWORD
         }
 
+        # Una identificacion generica
+        headers = {
+            'User-Agent':'MyAPI:/0.0.1'
+        }
+
+        # Enviar el request
+        auth_res = requests.post(REDDIT_ACCESS_URL, auth=auth, data=data, headers=headers)
+
+        # Ver la respuesta de la API
+        token = auth_res.json()['access_token']
+        headers['Authorization'] = f'bearer {token}'
+
+
+
         try:
-            response = requests.get(REDDIT_API_URL, params=params)
+            response = requests.get(f"{REDDIT_HOST}/r/{ticker}/new", headers=headers)
             response.raise_for_status()
-            return response.json().get("data", [])
+
+            data = [] # Inicializo
+
+            # Iterar sobre los datos que recibimos de la consulta
+            for post in response.json()['data']['children']:
+                # poner los datos en el df
+                data.append([
+                    post['data']['subreddit'],
+                    post['data']['title'],
+                    post['data']['selftext'],
+                    post['data']['upvote_ratio'],
+                    post['data']['ups'],
+                    post['data']['downs'],
+                    post['data']['score']
+                    ])
+
+
+            cols = ['subreddit', 'title', 'selftext', 'upvote_ratio', 'ups', 'downs', 'score']
+
+            return [data, cols]
         except requests.exceptions.RequestException as e:
             print(f"Error fetching Reddit posts: {e}")
             return []
